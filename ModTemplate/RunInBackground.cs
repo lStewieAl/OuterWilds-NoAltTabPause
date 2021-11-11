c5277ea8de30787cf36031feedd8c136651e3148
BuildTools <lStewieAl@gmail.com>
2021-11-11 21:27:36 +0000

    Initial commit

---
+++ ModTemplate/RunInBackground.cs
@@ -0,0 +1,13 @@
+﻿using OWML.ModHelper;
+using UnityEngine;
+
+namespace lStewieAl
+{
+    public class RunInBackground : ModBehaviour
+    {
+        private void Start()
+        {
+            Application.runInBackground = true;
+        }
+    }
+}
